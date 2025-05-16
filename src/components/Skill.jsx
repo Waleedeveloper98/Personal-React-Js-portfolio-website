a7f60af2ba606eec35068367afb2948e6958f76d
Waleed <waleedeveloper100@gmail.com>
2025-05-16 08:31:18 +0500

    minor settings done

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -9,7 +9,7 @@ const Skill = ({ skillText, colStart }) => {
     >
       <div
         className="absolute inset-0 bg-[linear-gradient(to_right,_rgba(0,123,255,0.5)_0%,_rgba(0,123,255,0.5)_85%,_rgba(0,249,166,0.5)_100%)]
-       -z-10 rounded-sm transition-all duration-500 group-hover:blur-sm group-active:scale-110"
+       -z-10 rounded-sm transition-all duration-500 group-hover:blur-sm"
       ></div>
 
       <div className="bg-white h-10 sm:h-12 flex items-center justify-center font-[roboto] w-full rounded-sm text-center sm:text-lg lg:text-xl lg:h-16 lg:font-medium cursor-pointer transition-all duration-1000">
